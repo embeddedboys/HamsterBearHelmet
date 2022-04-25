d01ee226610514983151dcee30ee2b01471b0960
IotaHydrae <writeforever@foxmail.com>
2022-04-26 03:09:08 +0800

    format files

--- ui/ui_handlers/charts_handlers.c
+++ ui/ui_handlers/charts_handlers.c
@@ -11,43 +11,45 @@
 extern lv_obj_t *ui_ChartSensorValue;
 pthread_t tid_chart_sensors;
 
-static void *ui_charts_sensor_update_thread_function(void *privdata)
+static void *ui_charts_sensor_update_thread_function( void *privdata )
 {
-    lv_chart_series_t * ser1 = (lv_chart_series_t * )privdata;
-
-    while(1) {
-        lv_chart_set_next_value(ui_ChartSensorValue, ser1, rand() % 10 + 20);
-        lv_chart_refresh(ui_ChartSensorValue); /*Required after direct set*/
-        sleep(1);
+    lv_chart_series_t *ser1 = ( lv_chart_series_t * )privdata;
+    
+    while( 1 ) {
+        lv_chart_set_next_value( ui_ChartSensorValue, ser1, rand() % 10 + 20 );
+        lv_chart_refresh( ui_ChartSensorValue ); /*Required after direct set*/
+        sleep( 1 );
     }
-
+    
     return NULL;
 }
 
-static int ui_charts_init(void)
+static int ui_charts_init( void )
 {
     int ret;
-
+    
     /*Add two data series*/
-    pr_debug("%s\n", __func__);
-    lv_chart_set_range(ui_ChartSensorValue, LV_CHART_AXIS_PRIMARY_Y, 0, 50);
-    lv_chart_set_axis_tick(ui_ChartSensorValue, LV_CHART_AXIS_PRIMARY_Y, 10, 5, 6, 2, true, 20);
-
-    lv_chart_series_t * ser1 = lv_chart_add_series(ui_ChartSensorValue, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
+    pr_debug( "%s\n", __func__ );
+    lv_chart_set_range( ui_ChartSensorValue, LV_CHART_AXIS_PRIMARY_Y, 0, 50 );
+    lv_chart_set_axis_tick( ui_ChartSensorValue, LV_CHART_AXIS_PRIMARY_Y, 10, 5, 6,
+                            2, true, 20 );
+                            
+    lv_chart_series_t *ser1 = lv_chart_add_series( ui_ChartSensorValue,
+                                                   lv_palette_main( LV_PALETTE_RED ), LV_CHART_AXIS_PRIMARY_Y );
     // lv_chart_series_t * ser2 = lv_chart_add_series(ui_ChartSensorValue, lv_palette_main(LV_PALETTE_GREEN), LV_CHART_AXIS_SECONDARY_Y);
-
+    
     /*Set the next points on 'ser1'*/
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 17);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 20);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 23);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 25);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 27);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 26);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 23);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 19);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 12);
-    lv_chart_set_next_value(ui_ChartSensorValue, ser1, 11);
-
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 17 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 20 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 23 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 25 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 27 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 26 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 23 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 19 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 12 );
+    lv_chart_set_next_value( ui_ChartSensorValue, ser1, 11 );
+    
     /*Directly set points on 'ser2'*/
     // ser2->y_points[0] = 90;
     // ser2->y_points[1] = 70;
@@ -59,32 +61,36 @@ static int ui_charts_init(void)
     // ser2->y_points[7] = 65;
     // ser2->y_points[8] = 65;
     // ser2->y_points[9] = 65;
-
-    ret = pthread_create(&tid_chart_sensors, NULL, ui_charts_sensor_update_thread_function, (void *)ser1);
-    if (ret != 0) {
-        pr_debug("%s: pthread_create failed\n", __func__);
+    
+    ret = pthread_create( &tid_chart_sensors, NULL,
+                          ui_charts_sensor_update_thread_function, ( void * )ser1 );
+                          
+    if( ret != 0 ) {
+        pr_debug( "%s: pthread_create failed\n", __func__ );
         return -1;
     }
-
+    
     return 0;
 }
 
-static int ui_charts_exit(void)
+static int ui_charts_exit( void )
 {
     int ret = -1;
-
-    ret = pthread_cancel(tid_chart_sensors);
-    if (ret != 0) {
-        pr_debug("%s: pthread_cancel failed\n", __func__);
+    
+    ret = pthread_cancel( tid_chart_sensors );
+    
+    if( ret != 0 ) {
+        pr_debug( "%s: pthread_cancel failed\n", __func__ );
         return -1;
     }
+    
     return 0;
 }
 
-static void ui_charts_update(void)
+static void ui_charts_update( void )
 {
-    lv_chart_refresh(ui_ChartSensorValue); /*Required after direct set*/
+    lv_chart_refresh( ui_ChartSensorValue ); /*Required after direct set*/
 }
 
 
-UI_HANDLER_REGISTER(ui_charts);
+UI_HANDLER_REGISTER( ui_charts );
--- ui/ui_handlers/statusbar_manager.c
+++ ui/ui_handlers/statusbar_manager.c
@@ -11,11 +11,11 @@
 struct statusbar_state {
     struct timeval tv;
     struct tm tm_res;
-
+    
     uint8_t hour;
     uint8_t minute;
     uint8_t second;
-
+    
     uint8_t wifi;
     uint8_t bt;
     uint8_t battry;
@@ -30,57 +30,59 @@ extern lv_obj_t *ui_LabelTime1;
 struct statusbar_state *g_state;
 pthread_t tid_status_bar;
 
-static void *ui_statusbar_update_thread_function(void *privdata)
+static void *ui_statusbar_update_thread_function( void *privdata )
 {
     char time_buf[10];
-    struct statusbar_state *state = (struct statusbar_state *)privdata;
-
+    struct statusbar_state *state = ( struct statusbar_state * )privdata;
+    
     // state->hour = state->tm_res.tm_hour;
     // state->minute = state->tm_res.tm_min;
     // state->second = state->tm_res.tm_sec;
-
-    while (1) {
-
+    
+    while( 1 ) {
+    
         /* time */
-        gettimeofday(&state->tv, NULL);
-        localtime_r(&state->tv.tv_sec, &state->tm_res);
+        gettimeofday( &state->tv, NULL );
+        localtime_r( &state->tv.tv_sec, &state->tm_res );
         state->minute = state->tm_res.tm_min;
-
-        snprintf(time_buf, sizeof(time_buf), "%02d:%02d", state->tm_res.tm_hour,
-                 state->tm_res.tm_min);
-
-        lv_label_set_text(ui_LabelTime, time_buf);
-        lv_label_set_text(ui_LabelTime1, time_buf);
-
+        
+        snprintf( time_buf, sizeof( time_buf ), "%02d:%02d", state->tm_res.tm_hour,
+                  state->tm_res.tm_min );
+                  
+        lv_label_set_text( ui_LabelTime, time_buf );
+        lv_label_set_text( ui_LabelTime1, time_buf );
+        
         /* battery */
-
+        
         /* switch */
-
-        sleep(60);
+        
+        sleep( 60 );
     }
-
+    
     return NULL;
 }
 
-static int ui_statusbar_init(void)
+static int ui_statusbar_init( void )
 {
-    pr_debug("%s\n", __func__);
-    g_state = (struct statusbar_state *)malloc(sizeof(struct statusbar_state));
-
-    pthread_create(&tid_status_bar, NULL, ui_statusbar_update_thread_function, (void *)g_state);
+    pr_debug( "%s\n", __func__ );
+    g_state = ( struct statusbar_state * )malloc( sizeof( struct
+                                                          statusbar_state ) );
+                                                          
+    pthread_create( &tid_status_bar, NULL, ui_statusbar_update_thread_function,
+                    ( void * )g_state );
     return 0;
 }
 
-static int ui_statusbar_exit(void)
+static int ui_statusbar_exit( void )
 {
-    free(g_state);
-    pthread_cancel(tid_status_bar);
-
+    free( g_state );
+    pthread_cancel( tid_status_bar );
+    
     return 0;
 }
 
-static void ui_statusbar_update(void)
+static void ui_statusbar_update( void )
 {
 }
 
-UI_HANDLER_REGISTER(ui_statusbar);
+UI_HANDLER_REGISTER( ui_statusbar );
